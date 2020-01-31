209052a29409848d00e2d3199ef6bda2dece10bb
Chris Manson <chris@manson.ie>
2020-01-31 14:27:49 +0000

    update using ember-cli-update and npm-check -u

--- index.js
+++ index.js
@@ -4,10 +4,8 @@ var Funnel = require('broccoli-funnel');
 const { join } = require('path');
 
 module.exports = {
-  name: 'empress-blog-attila-template',
-  included: function(/* app */) {
-    this._super.included.apply(this, arguments);
-  },
+  name: require('./package').name,
+
   treeForPublic: function() {
     return new Funnel(join(this.root, 'public'));
   }
